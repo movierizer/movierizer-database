92c34e99859c6ed950b4b03daeeb845430fc7358
s0nren <piard.hugo2004@gmail.com>
2025-05-14 17:37:43 +0200

    fix change type of user id

--- planDatabase.sql
+++ planDatabase.sql
@@ -19,7 +19,7 @@ CREATE TABLE movies (
 ----------------User tables---------------
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
